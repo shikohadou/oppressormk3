ff75337f9ceab5a7cc17e4f4b76a65a43b862b12
shikohadou <89039934+shikohadou@users.noreply.github.com>
2023-06-16 09:54:05 +0100

    Add files via upload

---
+++ vehicle.lua file/vehicle.lua
@@ -0,0 +1,10 @@
+    ['oppressor3'] = {
+        ['name'] = 'Oppressor Mark 3',
+        ['brand'] = 'custom',
+        ['model'] = 'oppressor3',
+        ['price'] = 0,
+        ['category'] = 'motorcycles',
+        ['categoryLabel'] = 'motorcycles',
+        ['hash'] = `oppressor3`,
+        ['shop'] = 'private',
+    },
